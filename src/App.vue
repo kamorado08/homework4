a37b690e60cf8d41aeb223713f5d6f6c2f380817
Karen Morado <khinojo7@stu.southtexascollege.edu>
2024-09-21 16:11:46 -0500

    Finish AppTransaction component

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
 import Header from './components/Header.vue';
 import Balance from './components/Balance.vue';
 import IncomeExpense from './components/IncomeExpense.vue';
+import AddTransaction from './components/AddTransaction.vue';
 import {ref, computed} from 'vue'
 
 //array 
@@ -46,5 +47,8 @@ const moneyOut = computed(()=>{
   <div class="container">
     <Balance :total="sum"></Balance>
     <IncomeExpense :income="moneyIn" :expense="moneyOut"></IncomeExpense>
+    <AddTransaction></AddTransaction>
   </div>
+
+
 </template>
