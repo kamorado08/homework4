67293e473b15036269c74fbebd5a7ce887ec3585
Karen Morado <khinojo7@stu.southtexascollege.edu>
2024-09-21 15:40:26 -0500

    finished Income Expense component

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
 import Header from './components/Header.vue';
 import Balance from './components/Balance.vue';
+import IncomeExpense from './components/IncomeExpense.vue';
 import {ref, computed} from 'vue'
 
 //array 
@@ -12,17 +13,38 @@ const transactions = ref([
 ])
 
 //x represents one element in the transaction
+//sum is a variable used to display the total sum of the array
 const sum = computed(()=>{
   return transactions.value.reduce((acc, x) =>{
     //running total 
     return acc+x.amount
   },0) 
 })
+
+const moneyIn = computed(()=>{
+  return transactions.value
+  .filter((x)=>x.amount>0)
+  .reduce((acc, x) =>{
+    //running total 
+    return acc+x.amount
+  },0) 
+})
+
+const moneyOut = computed(()=>{
+  return transactions.value
+  .filter((x)=>x.amount<0)
+  .reduce((acc, x) =>{
+    //running total 
+    return acc+x.amount
+  },0) 
+})
+
 </script>
 
 <template>
   <Header></Header>
   <div class="container">
     <Balance :total="sum"></Balance>
+    <IncomeExpense :income="moneyIn" :expense="moneyOut"></IncomeExpense>
   </div>
 </template>
